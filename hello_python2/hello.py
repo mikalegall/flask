f0ceae7b18096554e5b71987bcb53e42773a58bf
Mika Le Gall <mika.legall@live.fi>
2021-05-25 09:47:50 +0300

    hello python ja laajennus

---
+++ hello_python2/hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from animal import *
+from kissa import *
+
+app = Flask(__name__)
+
+
+#Main metodi suorittaa varsinaisen ohjelman
+def main():
+	eläin = Animal()
+	print(eläin.sano_jotain())
+	kisu = Kissa()
+	print(kisu.sano_jotain())
+	print(f"Kisu peri Animal-luokasta nimen {kisu.name}, jota ei ylikirjoitettu") 
+
+#Jos tiedostoa ajetaan suoraan komentoriviltä (eikä ole esim. importattu ipython3:seen) 
+if __name__ == "__main__":
+	#Suorita main-metodi
+	main()
