13c88c84ca004b755fbac68cbfdc7bf14537bb70
Mika Le Gall <mika.legall@live.fi>
2021-05-26 18:54:39 +0300

    error in line 73

--- technical_analysis/services/analytics.py
+++ technical_analysis/services/analytics.py
@@ -1,52 +1,64 @@
+# Tuodaan käytettäväksi data-analytiikan kirjasto pandas ja lyhennetään sitä kutsuttavaksi aliaksella pd
+# https://pandas.pydata.org/docs/user_guide/dsintro.html#dataframe
+import pandas as pd # sudo apt install python3-pandas
+
+# Tuodaan graafiseen esittämiseen matplotlib ja sen käyttöliittymäksi pyplot
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.html?highlight=pyplot#module-matplotlib.pyplot
+import matplotlib.pyplot as plt # sudo apt install python3-matplotlib && sudo apt install python3-seaborn
+
+
 # Tuodaan käytettäväksi internetistä datan noutaja esim. Yahoo Financesta valuuttakursseille
 # https://pydata.github.io/pandas-datareader/remote_data.html#forex
 # DataReadilla noudetuissa tiedoissa on aikaleima valmiina indeksissä
 import pandas_datareader as web
-# Tuodaan graafiseen esittämiseen matplotlib ja sen käyttöliittymäksi pyplot
-# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.html?highlight=pyplot#module-matplotlib.pyplot
-import matplotlib.pyplot as plt
-# Tuodaan käytettäväksi data-analytiikan kirjasto pandas ja lyhennetään sitä kutsuttavaksi aliaksella pd
-# https://pandas.pydata.org/docs/user_guide/dsintro.html#dataframe
-import pandas as pd
+###sudo apt install python3-pip
+###pip3 install pandas_datareader
+###conda install -c anaconda pandas-datareader
+###VS codessa Ctrl+P  ext install ms-python.python
+### pip install pandas-datareader
+#git clone https://github.com/pydata/pandas-datareader.git
+#cd pandas-datareader
+#sudo python3 setup.py install
+
 
 import os
 from pathlib import Path
 # Ajan käsittelyyn tarvittavia kirjastoja
-from datetime import datetime
+#from datetime import datetime
 import warnings
 warnings.filterwarnings('ignore')
 
 
-# One level up
-ROOT_DIR = Path(__file__).parent
+# Two level up
+ROOT_DIR = Path(__file__).parent.parent
 
-
-def analyze():
-    //TODO Copy+paste omasta Jupyter Notebookista
-
-    # Valitaan graafinen esittäminen muotoiltavaksi tietyllä tyylillä
-    plt.style.use('seaborn-whitegrid')
-
-
-//TODO Muuttujien nimien käsittely
+def analyze(target, short_rolling_mean, long_rolling_mean):
+    print("target = ", target, "short_rolling_mean = ", short_rolling_mean,  "long_rolling_mean = ", long_rolling_mean)
     # Hae kohde-etuuden tunniste noudetettavasta kohteesta
     # https://duckduckgo.com/?q=yahoo+finance+elisa
     # https://duckduckgo.com/?q=yahoo+finance+nordea+bull+dax+10
-    elisa = web.DataReader('ELISA.HE', start='2015-1-1', data_source='yahoo')
+    target_data = web.DataReader(target, start='2015-1-1', data_source='yahoo')
+    print("target_data = ", target_data)
+    # Valitaan graafinen esittäminen muotoiltavaksi tietyllä tyylillä
+    plt.style.use('seaborn-whitegrid')
 
     #Suurennetaan esitysaluetta oletuskoostaan suuremmaksi
-    elisa['Close'].plot(figsize=(14,6))
-
+    target_data['Close'].plot(figsize=(14,6))
     #Satunnaisvaihtelun tasaamiseen voidaan laskea liukuva keskiarvo
     # https://www.investopedia.com/articles/active-trading/052014/how-use-moving-average-buy-stocks.asp
     ##Liukuva keskiarvo voi olla singnaalina toimenpiteelle
     ##Jatkuvasti rullaava (liukuva, rolling) komento rolling() ottaa parametrikseen ajankohtien lukumäärän
     ###Lasketaan 50 viimeiselle päivälle jatkuvasti päivittyvänä (rullaavana, liukuvana) keskiarvo komennolla mean()
-    elisa['Close'].rolling(50).mean().plot()
-
+    target_data['Close'].rolling(int(short_rolling_mean)).mean().plot()
     #Samaan visualisointiin voidaan plotata (graafisesti esitettäväksi) useampia kaavioita samanaikaisesti
     ##Lasketaan Elisan osakkeen päätösluvulle 200 päivän liukuva keskiarvo
-    elisa['Close'].rolling(200).mean().plot()
+    target_data['Close'].rolling(int(long_rolling_mean)).mean().plot()
+    #Save figure
+    ##Figure https://tilastoapu.wordpress.com/2019/07/02/kuviot-ja-kaaviot-pythonilla/
+    ###Get current figure (plt.gct)
+    #### https://video.haaga-helia.fi/media/Data-analytiikka+Zoom-tallenne+21.4.2021/0_xj9pcn3d
+    plt.gcf().savefig(os.path.join(ROOT_DIR, 'img', 'rateanalysis.png'), bbox_inches = 'tight')
+    #Ilman lisäparametria bbox_inches='tight' saattaa kuvion reunoilta jäädä osa tallentumatta
 
 
 
@@ -55,20 +67,22 @@ def analyze():
     ##Luodaan DataFrameen uusi sarake nimeltä "Elisa %" ja sijoitettaan sen arvoksi
     ##taulukon "Close" sarakkeesta komennolla "pct_change()" laskettu prosentuaalinen muutos
     ##Muutos on verrattuna edelliseen eli tässä tapauksessa edelliseen pörssinkäyntipäivään
-    elisa['%'] = elisa['Close'].pct_change()
-
-
+    target_data['percent'] = target_data['Close'].pct_change()
     #Liitetään konkatenoinnilla juuri luodut muutosprosentti-sarakket kolmanteen taulukkoon
     #muutokset = pd.concat([elisa['Elisa %'], telia['Telia %']], axis = 1)
-    muutokset = elisa['%']
-
+    changes = target_data['percent']
+    print(changes)
     #Vuosittaisen volatiliteetin muutosprosentin keskihajonta jatkuvasti rullaava (liukuva, rolling)
     ##Komento rolling() ottaa parametrikseen ajankohtien lukumäärän
     ##Lasketaan vuoden kaupantekopäivien (252 päivää) muutosprosenteille jatkuvasti päivittyvänä (rullaavana, liukuvana)
     ##keskihajonta (standard deviation) komennolla std() ja skaalataan se neliöjuurella kertomalla vuosittaiseksi
     ##sekä esitetään se plotaten graafisena visualisointina (lopulta suurennetaan esitysaluetta oletuskoostaan suuremmaksi)
-    (muutokset['Elisa%'].rolling(252).std() * (252**0.5)).plot(label = 'Elisa', legend = True, figsize=(14,6))
+    (changes['percent'].rolling(252).std() * (252**0.5)).plot(label = target, legend = True, figsize=(14,6))
+    #Save figure
+    ##Figure https://tilastoapu.wordpress.com/2019/07/02/kuviot-ja-kaaviot-pythonilla/
+    ###Get current figure (plt.gct)
+    #### https://video.haaga-helia.fi/media/Data-analytiikka+Zoom-tallenne+21.4.2021/0_xj9pcn3d
+    plt.gcf().savefig(os.path.join(ROOT_DIR, 'img', 'volatilite.png'), bbox_inches = 'tight')
+    #Ilman lisäparametria bbox_inches='tight' saattaa kuvion reunoilta jäädä osa tallentumatta
 
-
-    //TODO Tallenna kuvatiedostona
-    #plt.gcf().savefig(os.path.join(ROOT_DIR, 'img', 'rateanalysis.png'), bbox_inches = 'tight')
+    return "done"
