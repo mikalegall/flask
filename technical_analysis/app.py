c5cf69d46f295a401287ea07474b07c118ba7b91
Mika Le Gall <mika.legall@live.fi>
2021-05-26 18:58:33 +0300

    clarify post method

--- technical_analysis/app.py
+++ technical_analysis/app.py
@@ -1,18 +1,38 @@
 "Stock and currency rate for technical analysis purposes"
 import time
-from flask import Flask, render_template
-import atexit
-import datetime
+from flask import Flask, render_template, request, redirect
+from services.analytics import analyze
+
+#import atexit
+#import datetime
 # https://bitbucket.org/kruutti/data-web-application/src/master/
 #from apscheduler.schedulers.background import BackgroundScheduler
-from analytics import analyze
 
 app = Flask(__name__)
+#Cross Site Request Forgery estämiseen
+app.secret_key = "mikalegall"
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/", methods=["GET"])
+def get_index():
     title = "Technical analysis tool"
+
     return render_template('index.html', title=title)
+
+@app.route("/", methods=["POST"])
+def post_index():
+
+    target = ""
+    short_rolling_mean = ""
+    long_rolling_mean = ""
+
+    if "target" in request.form:
+        target = request.form["target"]
+        short_rolling_mean = request.form["short_rolling_mean"]
+        long_rolling_mean = request.form["long_rolling_mean"]
+        results = analyze(target, short_rolling_mean, long_rolling_mean)
+        print("results = ", results)
+
+    return redirect("/rate")
 
 
 @app.route("/rate")
