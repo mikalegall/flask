09f7ffddeba3007c0427a3f8b8fc4a1e0a81f64c
Mika Le Gall <mika.legall@live.fi>
2021-05-26 16:22:46 +0300

    flask hello world

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -4,17 +4,5 @@ from flask import Flask
 #Flask-luokan konstruktori ottaa parametrikseen main-metodin
 app = Flask(__name__)
 
-#Endpoint
-@app.route('/nettiosoite')
-
-#Funktiot määritellään avainsanalla def ja lohko aloitetaan kaksoispisteellä
-def controller():
-	 #Funktion sisältämän lohkon sisennys on syntaksissa pakollista
-	 return "Moikka moi!"
-
-#Jos tiedostoa ajetaan suoraan komentoriviltä (eikä ole esim. importattu ipython3:seen) 
-if __name__ == "__main__":
-	#Käynnistä paikallinen kehityspalvelin
-	app.run(debug=True, port=8888)
-	#Käynnistyttyään vastaanottaa kutsun endpointissa
-    	#http://127.0.0.1:8888/nettiosoite
+#Käynnistä paikallinen kehityspalvelin
+app.run()
